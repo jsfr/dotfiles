9813cd4919f1c878ba5982ea538bffac9d49fb12
Jens Fredskov <jsfr@users.noreply.github.com>
2024-10-31 17:39:05 +0100

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,5 +2,8 @@ Invoke-Expression (&starship init powershell)
 
 $env:EDITOR = "C:\Users\JensFredskov\scoop\apps\neovim\current\bin\nvim.exe"
 
-Set-Alias -Name ls -value "eza -1 -F"
 Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
+
+function ls {
+    eza -1 -F $args
+}
