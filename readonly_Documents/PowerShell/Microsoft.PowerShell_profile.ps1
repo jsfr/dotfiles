81e6ac79134967d00b5d1107a1d6278cd0c99406
Jens Fredskov <jsfr@users.noreply.github.com>
2024-11-01 10:39:32 +0100

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,9 @@ $env:EDITOR = "C:\Users\JensFredskov\scoop\apps\neovim\current\bin\nvim.exe"
 
 Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
 
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+
 Remove-Item Alias:ls
 function ls {
     eza -1 -F $args
