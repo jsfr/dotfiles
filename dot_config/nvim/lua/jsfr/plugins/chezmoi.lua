8b86c542e8740e781c0c59886f46921fca3462f3
Jens Fredskov <jsfr@users.noreply.github.com>
2024-11-04 09:29:18 +0000

    Update .config/nvim/lua/jsfr/plugins/chezmoi.lua Change attributes of .stylua.toml

--- dot_config/nvim/lua/jsfr/plugins/chezmoi.lua
+++ dot_config/nvim/lua/jsfr/plugins/chezmoi.lua
@@ -5,5 +5,19 @@ return {
 		local chezmoi = require("chezmoi")
 
 		chezmoi.setup({})
+
+		local augroup = require("jsfr.utils.augroup")
+		augroup("ChezmoiGroup", function(autocmd)
+			autocmd({ "BufRead", "BufNewFile" }, {
+				pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
+				callback = function(ev)
+					local bufnr = ev.buf
+					local edit_watch = function()
+						require("chezmoi.commands.__edit").watch(bufnr)
+					end
+					vim.schedule(edit_watch)
+				end,
+			})
+		end)
 	end,
 }
