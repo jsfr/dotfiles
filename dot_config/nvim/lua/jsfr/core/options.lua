84d80dba4d7d9f8457ec66bf32a32f8e1a224312
Jens Fredskov <jsfr@users.noreply.github.com>
2024-11-12 21:54:58 +0100

    Never gonna tell a lie and hurt you

--- dot_config/nvim/lua/jsfr/core/options.lua
+++ dot_config/nvim/lua/jsfr/core/options.lua
@@ -56,7 +56,7 @@ set_options({
     wildmode = "list:longest,full",
     winminheight = 0,
     wrap = true,
-    undodir = "/tmp//,~/.undodir",
-    backupdir = "/tmp//,~/.backupdir",
-    directory = "/tmp//,~/.swapdir",
+    undodir = "/tmp//," .. vim.fs.normalize("~/.undodir"),
+    backupdir = "/tmp//," .. vim.fs.normalize("~/.backupdir"),
+    directory = "/tmp//," .. vim.fs.normalize("~/.swapdir"),
 })
