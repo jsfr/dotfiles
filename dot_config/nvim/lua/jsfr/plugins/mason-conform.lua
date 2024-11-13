ba3b2dc9ea20c562c1c6ea2f3abb2b0d418c03dd
Jens Fredskov <jsfr@users.noreply.github.com>
2024-11-13 17:37:32 +0100

    I made leetle mistake

--- dot_config/nvim/lua/jsfr/plugins/mason-conform.lua
+++ dot_config/nvim/lua/jsfr/plugins/mason-conform.lua
@@ -3,7 +3,7 @@ return {
     cond = not vim.g.vscode,
     dependencies = { "williamboman/mason.nvim", "stevearc/conform.nvim" },
     opts = {
-        ensure_installed = { "stylua", "shfmt", "taplo", "prettier", "csharpier" },
+        ensure_installed = { "stylua", "shfmt", "taplo", "prettier" },
         automatic_installation = false,
         quiet_mode = false,
     },
