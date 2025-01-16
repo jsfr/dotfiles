f7a405b821c889a6e4cee720dd17bde4d4cf9602
Jens Fredskov <jsfr@users.noreply.github.com>
2025-01-16 15:04:08 +0100

    Remove .config/nvim/lua/jsfr/plugins/mini/starter.lua

--- dot_config/nvim/lua/jsfr/plugins/mini/starter.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "echasnovski/mini.starter",
-    cond = not vim.g.vscode,
-    priority = 999,
-    opts = {
-        query_updaters = "abcdefghijklmnopqrstuvwxyz0123456789_.",
-    },
-}
