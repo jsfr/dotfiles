e8dd87ed9ec406d452b351f683e63fb9c3091262
Jens Fredskov <jsfr@users.noreply.github.com>
2025-01-28 15:00:18 +0000

    I am sorry

---
+++ dot_config/nvim/lua/jsfr/plugins/blink-cmp.lua
@@ -0,0 +1,67 @@
+return {
+    "saghen/blink.cmp",
+    cond = not vim.g.vscode,
+    dependencies = "rafamadriz/friendly-snippets",
+    version = "*",
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+        keymap = {
+            preset = "default",
+            ["<Tab>"] = {
+                function(cmp)
+                    if cmp.is_visible() then
+                        return cmp.select_next()
+                    elseif cmp.snippet_active() then
+                        return cmp.snippet_forward()
+                    else
+                        return false
+                    end
+                end,
+                "fallback",
+            },
+            ["<S-Tab>"] = {
+                function(cmp)
+                    if cmp.is_visible() then
+                        return cmp.select_prev()
+                    elseif cmp.snippet_active() then
+                        return cmp.snippet_backward()
+                    else
+                        return false
+                    end
+                end,
+                "fallback",
+            },
+            ["<CR>"] = {
+                "accept",
+                "fallback",
+            },
+        },
+
+        completion = {
+            list = {
+                selection = {
+                    preselect = false,
+                },
+            },
+        },
+
+        appearance = {
+            -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+            -- Useful for when your theme doesn't support blink.cmp
+            -- Will be removed in a future release
+            use_nvim_cmp_as_default = true,
+            -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+            -- Adjusts spacing to ensure icons are aligned
+            nerd_font_variant = "mono",
+        },
+
+        -- Default list of enabled providers defined so that you can extend it
+        -- elsewhere in your config, without redefining it, due to `opts_extend`
+        -- sources = {
+        -- default = { "lsp", "path", "snippets", "buffer" },
+        -- },
+    },
+    opts_extend = { "sources.default" },
+}
