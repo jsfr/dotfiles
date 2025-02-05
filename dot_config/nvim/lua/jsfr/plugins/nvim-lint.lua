2c6c1cbec6d745d46d409d9d5fb615b2d89b7d61
Jens Fredskov <jsfr@users.noreply.github.com>
2025-02-05 16:14:14 +0000

    Remove .config/nvim/lua/jsfr/plugins/mason-nvim-lint.lua Update .config/nvim/lua/jsfr/plugins/nvim-lint.lua

--- dot_config/nvim/lua/jsfr/plugins/nvim-lint.lua
+++ dot_config/nvim/lua/jsfr/plugins/nvim-lint.lua
@@ -1,34 +1,52 @@
 ---@module "lazy"
 ---@type LazySpec
 return {
-    "mfussenegger/nvim-lint",
-    cond = not vim.g.vscode,
-    ft = { "dockerfile", "fennel", "fish", "ghaction", "html" },
-    config = function()
-        local augroup = require("jsfr.utils.augroup")
-        local lint = require("lint")
-
-        lint.linters_by_ft = {
-            dockerfile = { "hadolint" },
-            fennel = { "fennel" },
-            fish = { "fish" },
-            ghaction = { "actionlint" },
-            html = { "htmlhint" },
-        }
-
-        vim.filetype.add({
-            pattern = {
-                [".*/.github/workflows/.*%.ya?ml"] = "yaml.ghaction",
+    {
+        "rshkarin/mason-nvim-lint",
+        cond = not vim.g.vscode,
+        dependencies = {
+            "williamboman/mason.nvim",
+            "mfussenegger/nvim-lint",
+        },
+        opts = {
+            ensure_installed = {
+                "hadolint",
+                "actionlint",
+                "htmlhint",
             },
-        })
+            automatic_installation = false,
+        },
+    },
+    {
+        "mfussenegger/nvim-lint",
+        cond = not vim.g.vscode,
+        ft = { "dockerfile", "fennel", "fish", "ghaction", "html" },
+        config = function()
+            local augroup = require("jsfr.utils.augroup")
+            local lint = require("lint")
 
-        augroup("NvimLintGroup", function(autocmd)
-            autocmd("BufWritePost", {
-                pattern = "*",
-                callback = function()
-                    lint.try_lint()
-                end,
+            lint.linters_by_ft = {
+                dockerfile = { "hadolint" },
+                fennel = { "fennel" },
+                fish = { "fish" },
+                ghaction = { "actionlint" },
+                html = { "htmlhint" },
+            }
+
+            vim.filetype.add({
+                pattern = {
+                    [".*/.github/workflows/.*%.ya?ml"] = "yaml.ghaction",
+                },
             })
-        end)
-    end,
+
+            augroup("NvimLintGroup", function(autocmd)
+                autocmd("BufWritePost", {
+                    pattern = "*",
+                    callback = function()
+                        lint.try_lint()
+                    end,
+                })
+            end)
+        end,
+    },
 }
