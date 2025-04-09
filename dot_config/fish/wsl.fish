72683538daa343b5cf0283ebcc61b29f643dcc5b
Jens Fredskov <jsfr@users.noreply.github.com>
2025-04-09 07:48:16 +0000

    I only play the games that I win at.

--- dot_config/fish/wsl.fish
+++ dot_config/fish/wsl.fish
@@ -1,4 +1,4 @@
-fish_add_path -P \
+fish_add_path -aP \
     "/mnt/c/Program Files/glzr.io/GlazeWM/cli" \
     /mnt/c/Users/JensFredskov/AppData/Local/Microsoft/WindowsApps \
     /mnt/c/Users/JensFredskov/scoop/shims \
@@ -6,10 +6,10 @@ fish_add_path -P \
     "/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0" \
     /mnt/c/WINDOWS/system32
 
-alias rebuild=sudo nixos-rebuild switch
+alias rebuild="sudo nixos-rebuild switch"
 alias search=nix-search
 alias dn=dotnet
 alias wg=winget.exe
-alias wcopy=win32yank.exe -i
-alias wpaste=win32yank.exe -o
-alias apicli=dotnet ./artifacts/bin/Api.Client.Cli/debug/Api.Client.Cli.dll
+alias wcopy="win32yank.exe -i"
+alias wpaste="win32yank.exe -o"
+alias apicli="dotnet ./artifacts/bin/Api.Client.Cli/debug/Api.Client.Cli.dll"
