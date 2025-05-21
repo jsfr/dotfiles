8a33800ee25cbc402af4c4423f0a28d943d2acf9
Jens Fredskov <jsfr@users.noreply.github.com>
2025-05-21 09:20:56 +0000

    I would rather be playing Destiny 2.

---
+++ dot_config/nvim/lua/jsfr/plugins/markdown.lua
@@ -0,0 +1,25 @@
+---@module "lazy"
+---@type LazySpec
+return {
+    {
+        "lukas-reineke/headlines.nvim",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        config = true, -- or `opts = {}`
+    },
+    {
+        "folke/zen-mode.nvim",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+    },
+    {
+        "folke/twilight.nvim",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+    },
+}
