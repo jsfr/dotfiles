6113c5c4ae03e1c789c554cf2543dbc623bb0f16
Jens Fredskov <jsfr@users.noreply.github.com>
2025-05-28 13:59:43 +0000

    Something fixed

--- dot_config/nvim/lua/jsfr/plugins/hunk.lua
+++ dot_config/nvim/lua/jsfr/plugins/hunk.lua
@@ -2,6 +2,10 @@
 ---@type LazySpec
 return {
     "julienvincent/hunk.nvim",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "echasnovski/mini.icons",
+    },
     cmd = { "DiffEditor" },
     opts = {},
 }
